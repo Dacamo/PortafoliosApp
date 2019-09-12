77c586789bdee304030f9e2f6f8b3c7516e1d651
Cristian David Castro Montes <cridacamo@hotmail.com>
2019-09-12 10:39:32 -0500

    Project base and customize Arquitecture

---
+++ src/PortafoliosApp/Controllers/ValuesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PortafoliosApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        [HttpGet]
+        [ProducesResponseType(200)]
+        public async Task<ActionResult<IEnumerable<string>>> GetAllAsync()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        
+    }
+}
