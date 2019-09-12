7f11060ffc364adf4be55ba8b8f27063e2d09d4c
edwinosoriogaviria <edwinosoriogaviria@hotmail.com>
2019-09-12 16:58:00 -0500

    Added portafolio controller
    Co-Authored-By: Cristian David Castro Montes <castrom1998@gmail.com>

--- src/PortafoliosApp/Controllers/PortafoliosController.cs
+++ src/PortafoliosApp/Controllers/PortafoliosController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using PortafoliosApp.Domain.Behaviors;
+using PortafoliosApp.Domain.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +14,7 @@ namespace PortafoliosApp.Controllers
     public class PortafoliosController : ControllerBase
     {
         private readonly IPortafolioBehavior _portafolioBehavior;
+        private readonly IMapper _mapper;
         public PortafoliosController(IPortafolioBehavior portafolioBehavior)
         {
             _portafolioBehavior = portafolioBehavior;
@@ -19,9 +22,47 @@ namespace PortafoliosApp.Controllers
         }
         [HttpGet]
         [ProducesResponseType(200)]
-        public async Task<ActionResult> GetAllAsync()
+        public async Task<ActionResult<Portafolio>> CreateAsync(Portafolio portafolio)
         {
+            await _portafolioBehavior.CreateAsync(portafolio);
+            return CreatedAtAction(nameof(GetByIdAsync), new { id = portafolio.Id }, portafolio);
+        }
 
+        [HttpGet]
+        [ProducesResponseType(200)]
+        public async Task<List<Portafolio>> GetAllAsync()
+        {
+            return await _portafolioBehavior.GetAllAsync();
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        public async Task<ActionResult<Portafolio>> GetByIdAsync(int id)
+        {
+            var portafolioExistente = await _portafolioBehavior.GetByIdAsync(id);
+            if (portafolioExistente == null)
+            {
+                return NotFound();
+            }
+            return portafolioExistente;
+
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public async Task<ActionResult> UpdateAsync(int id, Portafolio portafolio)
+        {
+            var portafolioExistente = await _portafolioBehavior.GetByIdAsync(id);
+            if (portafolioExistente == null)
+            {
+                return NotFound();
+            }
+            _mapper.Map(portafolio, portafolioExistente);
+            await _portafolioBehavior.UpdateAsync(portafolioExistente);
+
+            return NoContent();
         }
     }
 }
