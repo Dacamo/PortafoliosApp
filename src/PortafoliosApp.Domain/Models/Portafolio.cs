3cd7f1fecf3ab645582b8e5b409e2ea4fde90e53
Cristian David Castro Montes <cridacamo@hotmail.com>
2019-09-13 16:59:30 -0500

    Modify Portafolio Model
    Co-Authored-By: Edwin Osorio <edwin1106@users.noreply.github.com>

--- src/PortafoliosApp.Domain/Models/Portafolio.cs
+++ src/PortafoliosApp.Domain/Models/Portafolio.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace PortafoliosApp.Domain.Models
 {
     public class Portafolio
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Descripcion { get; set;}
+        [Required]
+        public string Objetivo { get; set; }
+        [Required]
         public DateTime FechaInicio { get; set; }
     }
 }
