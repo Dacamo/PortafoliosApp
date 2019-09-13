99f8a0bd4a1832bfba9b94ef0b79f6e94d41f8f0
Cristian David Castro Montes <cridacamo@hotmail.com>
2019-09-13 12:21:59 -0500

    Adding new integration test
    Co-Authored-By: Edwin Osorio <edwin1106@users.noreply.github.com>

--- test/PortafoliosApp.Test/PortafoliosController.cs
+++ test/PortafoliosApp.Test/PortafoliosController.cs
@@ -1,7 +1,10 @@
-﻿using PortafoliosApp.Test.Utils;
+﻿using Newtonsoft.Json;
+using PortafoliosApp.Domain.Models;
+using PortafoliosApp.Test.Utils;
 using System;
 using System.Collections.Generic;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
@@ -20,6 +23,31 @@ namespace PortafoliosApp.Test
             // Validar
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
             Assert.Equal("[]", content);
+        }
+
+        [Fact]
+        public async Task Post_Portafolio_RetornaPortafolioCreado()
+        {
+            // Preparar
+            var portafolio = new Portafolio
+            {
+               Id = 1,
+               Descripcion = "descripcion",
+               FechaInicio = DateTime.Parse("jan 1, 2009")
+
+            };
+
+            // Ejecutar
+            var response = await _client.PostAsJsonAsync("/api/portafolios", portafolio);
+            var content = await response.Content.ReadAsStringAsync();
+            var nuevoPortafolio = JsonConvert.DeserializeObject<Portafolio>(content);
+
+            // Validar
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+            Assert.Equal(1, nuevoPortafolio.Id);
+            Assert.Equal("descripcion", nuevoPortafolio.Descripcion);
+            Assert.Equal(DateTime.Parse("jan 1, 2009"), nuevoPortafolio.FechaInicio);
+           
         }
 
     }
