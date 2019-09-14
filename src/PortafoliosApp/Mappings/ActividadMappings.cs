7842d35fcbb74d926dec186d2eb93dc9318bb922
edwin1106 <30908546+edwin1106@users.noreply.github.com>
2019-09-14 12:13:29 -0500

    Added Actividad mappings
    Co-Authored-By: Cristian David Castro Montes <castrom1998@gmail.com>

---
+++ src/PortafoliosApp/Mappings/ActividadMappings.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using PortafoliosApp.Commands;
+using PortafoliosApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortafoliosApp.Mappings
+{
+    public class ActividadMappings: Profile
+    {
+        public ActividadMappings()
+        {
+            CreateMap<ActualizarActividad, Actividad>();
+            CreateMap<CrearActividad, Actividad>();
+        }
+    }
+}
