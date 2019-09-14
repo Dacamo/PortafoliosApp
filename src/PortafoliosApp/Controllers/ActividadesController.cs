ce530911fc4ab98c7b934a841e3df5ffc00e0f42
edwin1106 <30908546+edwin1106@users.noreply.github.com>
2019-09-14 12:43:13 -0500

    Added Actividades Controller
    Co-Authored-By: Cristian David Castro Montes <castrom1998@gmail.com>

---
+++ src/PortafoliosApp/Controllers/ActividadesController.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PortafoliosApp.Commands;
+using PortafoliosApp.Domain.Behaviors;
+using PortafoliosApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortafoliosApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActividadesController : ControllerBase
+    {
+        private readonly IActividadBehavior _actividadBehavior;
+        private readonly IMapper _mapper;
+
+        public ActividadesController(IActividadBehavior actividadBehavior, IMapper mapper)
+        {
+            _actividadBehavior = actividadBehavior;
+            _mapper = mapper;
+        }
+        [HttpPost]
+        public async Task<ActionResult<Actividad>> CreateAsync(CrearActividad crearActividad)
+        {
+            var actividad = _mapper.Map<Actividad>(crearActividad);
+
+            await _actividadBehavior.CreateAsync(actividad);
+
+            return CreatedAtAction(nameof(GetByIdAsync), new { id = actividad.Id }, actividad);
+        }
+        [HttpGet]
+        public async Task<List<Actividad>> GetAllAsync()
+        {
+            return await _actividadBehavior.GetAllAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Actividad>> GetByIdAsync(int id)
+        {
+            var actividadExistente = await _actividadBehavior.GetByIdAsync(id);
+
+            if(actividadExistente == null)
+                return NotFound();
+
+            return actividadExistente;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateAsync(int id, ActualizarActividad actualizarActividad)
+        {
+            var actividad = await _actividadBehavior.GetByIdAsync(id);
+
+            if(actividad == null)
+                return NotFound();
+
+            _mapper.Map(actualizarActividad, actividad);
+            await _actividadBehavior.UpdateAsync(actividad);
+
+            return NoContent();
+        }
+
+    }
+}
