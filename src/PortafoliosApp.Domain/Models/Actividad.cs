87dfd1cbde4efbcdf5f11dc073582896ecbc7bdb
edwin1106 <30908546+edwin1106@users.noreply.github.com>
2019-09-14 11:45:05 -0500

    Added Actividad model
    Co-Authored-By: Cristian David Castro Montes <castrom1998@gmail.com>

---
+++ src/PortafoliosApp.Domain/Models/Actividad.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PortafoliosApp.Domain.Models
+{
+    public class Actividad
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Descripcion { get; set; }
+        [Required]
+        public string Tutor { get; set; }
+        [Required]
+        public int Puntaje { get; set; }
+        [Required]
+        public DateTime Fecha { get; set; }
+
+
+
+    }
+}
